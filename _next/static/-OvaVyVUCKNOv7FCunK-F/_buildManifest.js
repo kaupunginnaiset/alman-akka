8bdf80b76d6b8cf098f79911a140aef7048a982d
lauravuo <lauravuo@users.noreply.github.com>
2022-04-28 18:06:56 +0000

    deploy: fa1d355f3f2de624771b6d9da53e1304d7bd42c0

--- _next/static/-OvaVyVUCKNOv7FCunK-F/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/997-f3ca802c52317cd6.js","static/css/7dd33e6c3fe4fbbf.css","static/chunks/pages/index-468db49197739782.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
