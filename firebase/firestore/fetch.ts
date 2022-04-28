764395ed6213e632d52659fb5158ae018bcf45ee
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2022-04-28 22:10:30 +0300

    Skip successive updates.

--- firebase/firestore/fetch.ts
+++ firebase/firestore/fetch.ts
@@ -1,3 +1,4 @@
+import * as fs from "fs";
 import * as fb from "firebase-admin";
 import { fetchEventsToFile } from "./db";
 
@@ -19,5 +20,13 @@ if (process.argv.length < 3) {
   });
   const folderName = `./data/${process.env.NODE_ENV || "development"}`;
   const lastFetchFilePath = `${folderName}/last-fetch.txt`;
+
+  const lastFetchTime = new Date(fs.existsSync(lastFetchFilePath) ? fs.readFileSync(lastFetchFilePath, "utf8") : 0);
+  if (new Date().getTime() - lastFetchTime.getTime() < 1000 * 60 * 5) {
+    // Skip too frequent fetching. Next nightly will pick up the change at latest.
+    console.log("Last fetch was less than 5 minutes ago. Update will be done with next trigger.");
+    process.exit(0);
+  }
+
   await fetchEventsToFile(lastFetchFilePath, folderName);
 })();
