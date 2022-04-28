a2964893beef8eae57e9084b86e10d8b6f860a66
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2022-04-28 21:39:42 +0300

    Reset development data.

--- data/development/index.js
+++ data/development/index.js
@@ -1,10 +1,5 @@
 import events0 from "./2021/06.json";
 import events1 from "./2021/07.json";
-import events2 from "./2022/04.json";
 
-const data = [
-...events0,
-...events1,
-...events2
-];
+const data = [...events0, ...events1];
 export default data;
