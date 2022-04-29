c528e96d9653749e9a13e887d98dbcf93249c865
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2022-04-29 07:57:22 +0300

    Fix creds file path.

--- components/pages/Homepage/index.tsx
+++ components/pages/Homepage/index.tsx
@@ -3,12 +3,16 @@ import styles from "../../../styles/Home.module.css";
 import data, { EventFromJSON } from "../../../data";
 import { EventCard } from "./EventCard";
 
+interface Event extends EventFromJSON {
+  id: string;
+}
+
 export const Frontpage = () => {
   return (
     <>
       <h1 className={styles.title}>Tapahtumat</h1>
       <div className={styles.events}>
-        {data.map((item: EventFromJSON) => (
+        {data.map((item: Event) => (
           <EventCard key={item.id} event={item} />
         ))}
       </div>
